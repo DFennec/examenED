83fc190d83e07fb484beab05f177cd0fbd7abdd4
ÁlvaroLozanoGarcía <alg888999@gmail.com>
2024-03-06 18:43:51 +0100

    Primer commit RA2[C

--- src/examen/Principal.java
+++ src/examen/Principal.java
@@ -4,5 +4,6 @@ public class Principal {
 public static void main(String[] args) {
 	System.out.println("Hola a tod@s");
 	System.out.println("Cambios para provocar un fast-forward");
+	System.out.println("Cambios en RA2");
 }
 }
