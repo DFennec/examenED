0a07349b117b0680a6c934fd40b3e1deaeb1cff5
ÁlvaroLozanoGarcía <alg888999@gmail.com>
2024-03-06 18:34:34 +0100

    Commit 1 de RA1

--- src/examen/Principal.java
+++ src/examen/Principal.java
@@ -3,5 +3,6 @@ package examen;
 public class Principal {
 public static void main(String[] args) {
 	System.out.println("Hola a tod@s");
+	System.out.println("Cambios para provocar un fast-forward");
 }
 }
